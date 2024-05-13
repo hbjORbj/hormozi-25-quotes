a209926d5a07036bfd9a5c7eb45a20576287127a
Benny Joo <sldisek783@gmail.com>
2024-05-13 08:43:28 +0100

    fix lint error

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className='hero'>
       <div className='hero-content text-left'>
         <h1 className='text-5xl font-bold font-black font-mono'>
-          Alex Hormozi's 25 best quotes, as voted on by the internet
+          Alex Hormozi&apos;s 25 best quotes, as voted on by the internet
         </h1>
       </div>
     </div>
